771f29168db3e03756aee8b4fd432913e3ee86ab
nickolss <nickolasmaiaraujo@gmail.com>
2025-01-07 16:27:17 -0300

    feat: Added spring boot project strucure using spring initializy

---
+++ design-patterns-spring/src/main/java/com/nickolas/design_patterns_spring/DesignPatternsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nickolas.design_patterns_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternsSpringApplication.class, args);
+	}
+
+}
